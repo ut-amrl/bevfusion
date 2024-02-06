f0843598298accf6d92560f56041b5e9599759df
arnavbagad <arnav.bagad@gmail.com>
2024-02-06 14:50:28 -0600

    Code works on Nuscenes dataset

--- test_nuscenes.sh
+++ test_nuscenes.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+python setup.py develop
 torchpack dist-run -np 1 python tools/test.py configs/nuscenes/det/transfusion/secfpn/camera+lidar/swint_v0p075/convfuser.yaml pretrained/bevfusion-det.pth --eval bbox
