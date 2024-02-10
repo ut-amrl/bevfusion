ab527bb382263cd35603bb3b9fd53b2c06213bb8
Arsh Gamare <arsh.gamare@utexas.edu>
2024-02-09 18:25:22 -0600

    Added img_to_vid.py and updated visualize.sh

---
+++ img_to_vid.py
@@ -0,0 +1,22 @@
+import cv2
+import os
+from tqdm import tqdm
+
+frame_directory = 'viz/lidar'
+video_path = 'viz/nuscenes_lidar.avi'
+frame_rate = 30  # frames per second
+resolution = (831, 831)  # Adjust based on your frame size
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter(video_path, fourcc, frame_rate, resolution)
+
+for frame_name in tqdm(sorted(os.listdir(frame_directory))):
+    frame_path = os.path.join(frame_directory, frame_name)
+    frame = cv2.imread(frame_path)
+    if frame is not None:
+        # Resize frame to match video resolution, if necessary
+        resized_frame = cv2.resize(frame, resolution)
+        out.write(resized_frame)
+
+out.release()
